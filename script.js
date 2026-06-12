c3d61d00e5fcf294a04d6468ef7347c5522b3c2e
shiraori11 <shiraori@proton.me>
2026-06-12 11:26:49 +0800

    Added Datas to Book Object

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 let library = [];
 
-function Book(bookName, bookDesc, bookID) {
+function Book(title, description, ID, author, status, pages) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call this Object");
   }
   
-  this.title = bookName;
-  this.description = bookDesc;
-  this.ID = bookID;
+  this.title = title;
+  this.description = description;
+  this.ID = ID;
+  this.author = author;
+  this.status = status;
+  this.pages = pages;
 }
 
 const libraryInterface = {
   title: document.querySelector("#title"),
   description: document.querySelector("#description"),
   addButton: document.querySelector("#addBook"),
+  author: document.querySelector("#bookAuthor"),
+  status: document.querySelector("#readStatus"),
+  pages: document.querySelector("#bookPages"),
   libraryContainer: document.querySelector("#library"),
 }
 
@@ -34,9 +40,16 @@ function addBookToLibrary() {
   const title = libraryInterface.title.value;
   const description = libraryInterface.description.value;
   const id = self.crypto.randomUUID();  
+  const author = libraryInterface.author.value;
+  const status = libraryInterface.status.value;
+  const pages = libraryInterface.pages.value;
   
-  const newBook = new Book(title, description, id);
+  const newBook = new Book(title, description, id, author, status, pages);
   library.push(newBook);
+
+  console.log(newBook);
+  console.log(library);
+  
   return newBook;
 }
 
@@ -53,10 +66,24 @@ function bookContainer(book) {
   const bookDiv = document.createElement("div");
   bookDiv.setAttribute("class", "book");
   bookDiv.setAttribute("data-book-id", book.ID);
-  const bookContent = document.createTextNode(`Title: ${book.title} Description: ${book.description} ID: ${book.ID}`);
 
-  bookDiv.appendChild(bookContent);
+  const title = bookContainerPara(book.title, "class-test");
+  const description = bookContainerPara(book.description, "descClass");
+  const author = bookContainerPara(book.author, "authorClass");
+  const status = bookContainerPara(book.status, "statusClass");
+  const pages = bookContainerPara(book.pages, "pagesClass");
+
+  bookDiv.append(title, description, author, status, pages);
+
   return bookDiv;
+}
+
+function bookContainerPara(text, paraClass) {
+  const newPara = document.createElement("p");
+  newPara.textContent = text;
+  newPara.setAttribute("class", paraClass);
+
+  return newPara;
 }
 
 interfaceListeners();
